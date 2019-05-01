d93125e96294adab2ad5d2e142c6c20f4cf21e28
Ramazan Erdem <erdemuysal@yahoo.com>
2019-05-02 00:00:54 +0300

    Add files via upload

---
+++ tumor_detection.m
@@ -0,0 +1,35 @@
+close all;
+clear all;
+clc;
+
+im = imread('brain.jpg');
+bw = im2bw(im,0.7);
+label = bwlabel(bw);
+stats = regionprops(label,'Solidity','Area');
+density = [stats.Solidity];
+area = [stats.Area];
+high_dense_area = density>0.5;
+max_area = max(area(high_dense_area));
+tumor_label = find(area==max_area);
+tumor=ismember(label,tumor_label);
+se=strel('square',5);
+tumor = imdilate(tumor,se);
+
+figure(2);
+subplot(1,3,1);
+imshow(im,[]);
+title('Brain MRI');
+subplot(1,3,2);
+imshow(tumor,[]);
+title('Tumor Alone');
+[B,L]=bwboundaries(tumor,'noholes');
+subplot(1,3,3);
+imshow(im,[]);
+hold on
+
+for i=1:length(B)
+    plot(B{i}(:,2),B{i}(:,1), 'y' ,'linewidth',1.45);
+end
+
+title('Detected Tumor');
+hold off;
